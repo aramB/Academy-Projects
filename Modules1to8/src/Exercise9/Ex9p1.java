978260a0330c112c1bcc1f43b5bfcc00649754f3
Aram Barzanjeh <aramiB@Arams-MacBook-Pro.local>
2017-10-02 20:57:30 +0200

    add Ex9p1

--- Modules1to8/src/Exercise9/Ex9p1.java
+++ Modules1to8/src/Exercise9/Ex9p1.java
@@ -1,7 +1,16 @@
 package Exercise9;
 
+import java.time.*;
+
 public class Ex9p1 {
 	public static void main(String[] args) {
-		
+		LocalDate lastRunDate = LocalDate.of(2016, 11, 11);
+		LocalDate today = LocalDate.of(2016, 11, 16);
+		if (lastRunDate.getDayOfMonth() - today.getDayOfMonth()<=7){
+			System.out.println("Not time yet!");
+		}
+		else {
+			System.out.println("Time to run again!");
+		}
 	}
 }
