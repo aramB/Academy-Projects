163f2cf51b24a8363a9d4b494a84778dc1aef1a3
aramiB <aramnr@yahoo.com>
2017-11-07 09:16:03 +0100

    update src

--- Modules1to8/src/testProgramming/Lanterna.java
+++ Modules1to8/src/testProgramming/Lanterna.java
@@ -7,13 +7,17 @@ import com.googlecode.lanterna.terminal.Terminal;
 public class Lanterna
 {
 	public static void main(String[] args) {
-		Screen screen = TerminalFacade.createScreen();
+		/*Screen screen = TerminalFacade.createScreen();
 		screen.startScreen();
 		for (int i = 0; i < 99; i++) {
 			for (int j = 0; j < 29; j++) {
 				screen.putString(40, 10, "Hello World!", Terminal.Color.WHITE, Terminal.Color.GREEN);
 			}
 		}
-		screen.refresh();
+		screen.refresh();*/
+		int a = 10;
+		while(a-->5){
+			System.out.println(a);
+		}
 	}
 }
