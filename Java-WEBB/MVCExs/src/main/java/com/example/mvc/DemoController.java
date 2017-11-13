a69921271cf56a1c5b9705832d58ac43ec774d8e
aramiB <aramnr@yahoo.com>
2017-11-13 10:52:11 +0100

    update democontroll

--- Java-WEBB/MVCExs/src/main/java/com/example/mvc/DemoController.java
+++ Java-WEBB/MVCExs/src/main/java/com/example/mvc/DemoController.java
@@ -2,6 +2,7 @@ package com.example.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,15 +15,29 @@ public class DemoController
 		return modelAndView;
 	}
 
-	@GetMapping("/ok1")
+	@GetMapping("/ok")
 	public ModelAndView helloo(){
 		ModelAndView modelAndView = new ModelAndView("helloo");
 		return modelAndView.addObject("name", "Aram");
 	}
 
-	@GetMapping("/ok")
+	@GetMapping("/hello3")
 	public ModelAndView hello(@RequestParam (/*name = "param", */required = false, defaultValue = "Human") String name){
 		ModelAndView modelAndView = new ModelAndView("helloo");
 		return modelAndView.addObject("name", name);
+	}
+
+	@GetMapping("/hello4/{name}")
+	public ModelAndView hello4(@PathVariable String name){
+		ModelAndView modelAndView = new ModelAndView("helloo");
+		return modelAndView.addObject("name", name);
+	}
+
+	@GetMapping("/hello5/{name}")
+	public ModelAndView hello5(@PathVariable String name){
+		ModelAndView modelAndView = new ModelAndView("hello5");
+		User user = new User();
+		user.setUsername(name);
+		return modelAndView.addObject("user", user);
 	}
 }
---
+++ Java-WEBB/MVCExs/src/main/java/com/example/mvc/User.java
@@ -0,0 +1,14 @@
+package com.example.mvc;
+
+public class User
+{
+	private String username;
+
+	public void setUsername(final String username) {
+		this.username = username;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+}
